b332c5277d2e18deba5aa57ea50ded6211829344
Refilwe15 <mrefilwe880@gmail.com>
2025-11-06 10:12:00 +0200

    the project is now connected to the database

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,25 @@
-import { Pool } from 'pg'
-import dotenv from "dotenv"
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database : process.env.DB_DATA,
-    password : process.env.PASSWORD,
-    port: parseInt(process.env.PORT || "5432"),
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || "5432")
 });
-export const query = (text: string ,params?: any[]) => pool.query(text,params)
 
+export const query = (text: string, params?: any[]) => pool.query(text, params);
+
+export const testDbConnection = async () => {
+  try {
+    const client = await pool.connect();
+    console.log("Database connection successful");
+    client.release();
+  } catch (error) {
+    console.error("Unable to connect to the database: ", error);
+    process.exit(1);
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
+import { testDbConnection } from "./config/database";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
 
-app.listen(PORT, () =>{
-    console.log(`Server is running on https://localhost:${PORT}`);
-    
-})
+testDbConnection();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
